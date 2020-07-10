b67591103a95130ce4826caa2127f1a63b4a19d4
shaikhamaan <62073338+shaikhamaan@users.noreply.github.com>
2020-07-10 16:26:33 +0530

    Added new features to the program

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -1,7 +1,9 @@
-var arr = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for(var names in arr){
-    if(arr[names].toLowerCase().charAt(0)=='j')
-        console.log('Good Bye ' + arr[names]);
-    else
-        console.log('Hello ' + arr[names]);
+function exe() {
+    var arr = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var names in arr) {
+        if (arr[names].toLowerCase().charAt(0) == 'j')
+            document.write("Good Bye " + arr[names] + "| |");
+        else
+            document.write("Hello " + arr[names] + "| |");
+    }
 }
